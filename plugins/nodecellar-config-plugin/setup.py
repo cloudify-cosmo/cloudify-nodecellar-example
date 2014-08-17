2f2501dcea7baad12ea6c078d970bcdf4f3a9fae
opencm <limor@gigaspaces.com>
2014-08-17 23:34:47 +0300

    Bump version to 3.1m2 / 1.1m2

--- plugins/nodecellar-config-plugin/setup.py
+++ plugins/nodecellar-config-plugin/setup.py
@@ -8,7 +8,7 @@ PLUGINS_COMMON = "https://github.com/cloudify-cosmo/cloudify-plugins-common/tarb
 setup(
     zip_safe=True,
     name='nodecellar-config-plugin',
-    version='3.1a1',
+    version='3.1a2',
     author='uric',
     author_email='uri@gigaspaces.com',
     packages=[
