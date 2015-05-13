1989b56afc2f5802ff357b8cee3087aceda94fec
Denis Pauk <pauk.denis@gmail.com>
2015-05-13 13:29:45 +0300

    CFY-2693: little improve code style

--- scripts/withoutagent/install.py
+++ scripts/withoutagent/install.py
@@ -8,23 +8,17 @@ def _run(command):
 
 def install_mongo(config):
     ctx.logger.info("Config: " + str(config))
-    script = []
-    script.append(
-        'sudo sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 2>&1'
-    )
-    script.append(
-        'echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list 2>&1'
-    )
-    script.append('sudo apt-get update 2>&1')
-    script.append('sudo apt-get install -y mongodb-org 2>&1')
-    # enable access from any ip
-    # by default access blocked to localhost
-    script.append(
-        'sudo sed "s/bind_ip = /#bind_ip = /g" -i /etc/mongod.conf'
-    )
-    script.append('sudo initctl stop mongod')
-    script.append('sudo initctl start mongod')
-    _run("\n".join(script))
+    _run("""
+sudo sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 2>&1
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list 2>&1
+sudo apt-get update 2>&1
+sudo apt-get install -y mongodb-org 2>&1
+# enable access from any ip
+# by default access blocked to localhost
+sudo sed "s/bind_ip = /#bind_ip = /g" -i /etc/mongod.conf
+sudo initctl stop mongod
+sudo initctl start mongod
+    """)
 
 def _generate_service(mongodb_host):
     return [
@@ -48,21 +42,25 @@ def _generate_service(mongodb_host):
 def install_node_js(config):
     ctx.logger.info("Config: " + str(config))
     script = []
-    script.append("sudo apt-get install python-software-properties -q -y 2>&1")
-    script.append("sudo apt-add-repository ppa:chris-lea/node.js -y 2>&1")
-    script.append("sudo apt-get update 2>&1")
-    script.append("sudo apt-get install nodejs make g++ wget -q -y 2>&1")
-    script.append("wget https://github.com/cloudify-cosmo/nodecellar/archive/master.tar.gz 2>&1")
-    script.append("tar -xvf master.tar.gz")
-    script.append("cd nodecellar-master && npm update")
+    script.append("""
+sudo apt-get install python-software-properties -q -y 2>&1
+sudo apt-add-repository ppa:chris-lea/node.js -y 2>&1
+sudo apt-get update 2>&1
+sudo apt-get install nodejs make g++ wget -q -y 2>&1
+wget https://github.com/cloudify-cosmo/nodecellar/archive/master.tar.gz 2>&1
+tar -xvf master.tar.gz
+cd nodecellar-master && npm update
+    """)
     # create service config
     service = _generate_service(config.get("mongo", "localhost"))
     script.append("rm -f /home/ubuntu/nodecellar.conf")
     for service_str in service:
         script.append('echo "' + service_str + '" >> /home/ubuntu/nodecellar.conf')
     # create init file
-    script.append("sudo cp /home/ubuntu/nodecellar.conf /etc/init/nodecellar.conf")
-    script.append("sudo chown root:root /etc/init/nodecellar.conf")
-    # run service
-    script.append('sudo initctl start nodecellar')
+    script.append("""
+sudo cp /home/ubuntu/nodecellar.conf /etc/init/nodecellar.conf
+sudo chown root:root /etc/init/nodecellar.conf
+# run service
+sudo initctl start nodecellar
+    """)
     _run("\n".join(script))
