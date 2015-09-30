4e938b3b230ca344c44e246416806ecb6fc5d629
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-09-30 16:37:40 +0300

    fix(simple): hard-code mongo ip to localhost
    CFY-3685

--- scripts/mongo/set-mongo-url.sh
+++ scripts/mongo/set-mongo-url.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-ctx source instance runtime_properties mongo_ip_address $(ctx target instance host_ip)
+if [ "$mongo_ip_address" = "" ];then
+    mongo_ip_address=$(ctx target instance host_ip)
+fi
+
+ctx source instance runtime_properties mongo_ip_address "$mongo_ip_address"
+
 ctx source instance runtime_properties mongo_port $(ctx target node properties port)
