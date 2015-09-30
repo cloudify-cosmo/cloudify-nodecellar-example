bfb551d803654fa12c2641b65a426e748beaf3d7
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-09-30 18:37:58 +0300

    fix(simple): simplify default behavior
    CFY-3685

--- scripts/mongo/set-mongo-url.sh
+++ scripts/mongo/set-mongo-url.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-if [ "$mongo_ip_address" = "" ];then
-    mongo_ip_address=$(ctx target instance host_ip)
-fi
-
-ctx source instance runtime_properties mongo_ip_address "$mongo_ip_address"
+ctx source instance runtime_properties mongo_ip_address ${mongo_ip_address}
 
 ctx source instance runtime_properties mongo_port $(ctx target node properties port)
