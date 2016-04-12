5482284d93ca2b1a9b4e1afea807f075bc55f218
David Ginzbourg <davidginzbourg@gmail.com>
2016-04-12 14:35:42 +0300

    Set mongo URL with runtime property

---
+++ scripts/mongo/set-mongo-url-dynamic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+if [ "$mongo_ip_address" = "" ];then
+    mongo_ip_address=$(ctx target instance runtime_properties ip_address)
+fi
+
+ctx source instance runtime_properties mongo_ip_address "$mongo_ip_address"
+ctx source instance runtime_properties mongo_port $(ctx target node properties port)
